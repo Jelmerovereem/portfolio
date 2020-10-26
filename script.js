ae283740dd2fa79bf1ea65205357e73294497506
Jelmerovereem <jelmer_overeem@hotmail.com>
2020-10-26 21:38:16 +0100

    updated

--- script.js
+++ script.js
@@ -57,3 +57,72 @@ window.onload = function () {
 	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
 	document.body.appendChild(css);
 };
+
+const counter = document.querySelector(".number-increment");
+const speed = 1000;
+
+window.onscroll = function() {updateCount()};
+
+function updateCount() {
+	if (window.pageYOffset > (counter.offsetTop - 800)) {
+		const target = +counter.dataset.count;
+		
+		const count = +counter.innerText;
+
+		const increment = target / speed;
+
+		if (count < target) {
+			counter.innerText = count + increment;
+			setTimeout(updateCount, 1);
+		} else {
+			count.innerText = target;
+		}		
+	}	
+}
+
+/*const projectsData = [
+{
+	titel: "Ecologisch verfbedrijf",
+	imgPaths: ["img/verfbedrijf2.jpg", "img/verfbedrijf3.jpg"]
+},
+{
+	titel: "Klusbedrijf",
+	imgPaths: []
+},
+{
+	titel: "KingStyle",
+	imgPaths: []
+}];
+
+const allProjects = document.querySelectorAll(".modalProject");
+const modal = document.querySelector(".outer-modal");
+const closeModalBtn = document.querySelector(".close");
+
+allProjects.forEach((projectHTML) => {
+	projectHTML.addEventListener("click", () => {
+		event.stopPropagation();
+		let title = projectHTML.dataset.title;
+		let projectObj = projectsData.find(obj => obj.titel === title);
+		let imgPaths = projectObj.imgPaths;
+		let modalHtml = `<span class="close">&times;</span><p>${projectHTML.querySelector("h4").innerText}</p>`;
+		imgPaths.forEach((image) => {
+			modalHtml += `<img src="${image}" alt="">`;
+		});
+		console.log(modalHtml)
+		modal.style.display = "block";
+		modal.querySelector(".modal-content").innerHTML = "";
+		modal.querySelector(".modal-content").insertAdjacentHTML("beforeend", modalHtml);
+		document.querySelector("body").style.overflow = "hidden";
+	})
+});
+
+function closeModal() {
+	modal.style.display = "none";
+	document.querySelector("body").style.overflow = "auto";
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+
+window.onclick = () => {
+	closeModal();	
+}*/
